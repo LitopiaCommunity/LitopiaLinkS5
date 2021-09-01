027bb3c4c372ea74984923bb96bfd8919d3617c9
LeChatonSousCoke <71146954+LeChatonSousCoke@users.noreply.github.com>
2021-09-01 22:00:34 +0200

    LITOPIA-31: Feat: Ajout d'une classe LitoStat qui recupere les statistiques des joueurs et peut les convertir sous JSON tout en les sauvegardant dans un fichier.

--- src/main/java/fr/litopia/main.java
+++ src/main/java/fr/litopia/main.java
@@ -1,20 +1,28 @@
 package fr.litopia;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
+
 
 public class main extends JavaPlugin {
+
+    // Instantiating
     @Override
     public void onLoad(){
-        System.out.println("[LitopiaLink] is loading !");
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.DARK_BLUE + "[LitopiaLink] is loading !");
     }
 
     @Override
     public void onEnable(){
-        System.out.println("[LitopiaLink] is Enable !");
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.DARK_BLUE + "[LitopiaLink] is Enable !");
+        // Event Listener
+        getServer().getPluginManager().registerEvents(new LitopiaListener(), this);
     }
 
     @Override
     public void onDisable(){
-        System.out.println("[LitopiaLink] is Disable !");
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.DARK_BLUE + "[LitopiaLink] is Disable !");
     }
+
 }
